da6511b663721f647f3524ea5b52f1e38cc8d12c
爱走位的KN_sword <3478099037@qq.com>
2023-05-07 12:51:40 +0800

    666

---
+++ board/script/data.js
@@ -0,0 +1,150 @@
+/*
+var data = {
+	ver_num: '0.0.0',
+	activeIndex: '1',
+	top_img_list: [{
+			id: 1,
+			idView: 'img/top_1.png',
+		},
+		{
+			id: 2,
+			idView: 'img/top_2.png'
+		},
+		{
+			id: 3,
+			idView: 'img/top_3.png'
+		},
+		{
+			id: 4,
+			idView: 'img/top_4.png'
+		}
+	],
+	chr_img_list: [{
+			id: 1,
+			idView: 'img/character/01.png',
+			chrName: "梦之彼七宫（夢ノ彼七宮）",
+			RMPN: "yu me no ka shi chi mi ya",
+			chrCV: "？？？",
+			detail: [{
+				id: 1,
+				content: "高中2年级文学部部长，性格天真活泼，内心却极度纤细脆弱，对喜欢的人会过分的依赖。"
+			}, {
+				id: 2,
+				content: "与主人公“蓝原逸”是青梅竹马，常常幻想着以后能成为他的新娘。"
+			}]
+		},
+		{
+			id: 2,
+			idView: 'img/character/02.png',
+			chrName: "濑户麻美（瀬戸マミ）",
+			RMPN: "se to ma mi",
+			chrCV: "？？？",
+			detail: [{
+					id: 1,
+					content: "樱花高等学校校长的爱女。成绩学年第一。"
+				}, {
+					id: 2,
+					content: "性格有些傲娇，十分可爱，还有着与外貌反差巨大的成熟稳重，同时也是隐藏的剑道高手。"
+				}, {
+					id: 3,
+					content: "传说中的天降与青梅？金毛与败犬？会发生怎样的碰撞呢？ "
+				}, {
+					id: 4,
+					content: "一切~请拭目以待~ "
+				}, {
+					id: 5,
+					content: "本作女主的优点，包括力速在内的全A面板。"
+				},
+				{
+					id: 6,
+					content: "本作女主的缺点，全A面板……"
+				}
+			]
+		},
+		{
+			id: 3,
+			idView: 'img/character/03.png',
+			chrName: "吉田沙梨（吉田さり）",
+			RMPN: "yo shi da sa ri",
+			chrCV: "？？？",
+			detail: [{
+				id: 1,
+				content: "与继母相依为命，虽然性格有些内向，但给人的印象倒不是很差，只是有些时候过于天真。"
+			}, {
+				id: 2,
+				content: "兴趣是游戏和插画，因为有着十分厉害的画技，常常会被其他社团拜托很多工作，经常在放学后忙到很晚才独自回家。"
+			}, {
+				id: 3,
+				content: "憧憬着魔法少女的奇迹力量，自诩拥有着魔法。"
+			}, {
+				id: 3,
+				content: "自从得知了自己的身世后，许下了想要见到亲生父母的愿望，成为了一位真正的魔法少女。"
+			}, {
+				id: 3,
+				content: "然而这份愿望却因意外并未完全实现，沙梨也因此陷入了迷茫之中，直到，与主人公的相遇。"
+			}]
+		},
+		{
+			id: 4,
+			idView: 'img/character/04.png',
+			chrName: "美树菱（美樹りん）",
+			RMPN: "mi ki ri n",
+			chrCV: "？？？",
+			detail: [{
+				id: 1,
+				content: "天文部部长，虽然早已毕业，但却并没有选择升学，在学校有自己独立的活动室，同时也兼任文学部的顾问，一切身份信息成迷。"
+			}, {
+				id: 2,
+				content: "性格十分温柔，常常有学生向她请教各种问题，她都会耐心的解答。"
+			}, {
+				id: 3,
+				content: "曾经也有学生好奇问过她为什么会选择不升学继续留在这个小镇，她只是回答，在等着一个人，之后便什么也没说了。"
+			}, ]
+		},
+		{
+			id: 5,
+			idView: 'img/character/05.png',
+			chrName: "稚名爱纱（椎名あいさ）",
+			RMPN: "shi i na ai sa",
+			chrCV: "？？？",
+			detail: [{
+				id: 1,
+				content: "樱花高等学校学生会会长。"
+			}, {
+				id: 2,
+				content: "性格认真果断，有些高傲难以接近，但实际都是装出来的，私底下非常喜欢音乐和游戏，对可爱的事物没有抵抗力，讨厌可怕的东西。"
+			}, {
+				id: 3,
+				content: "在初中部时，曾经有想要组建校园乐队的想法，但被朋友“背叛”，社团最终只剩下了她一个人。"
+			}, {
+				id: 4,
+				content: "因为无法满足学校规定的最低人数限制，社团被强制解散，爱纱也彻底放弃了乐队的梦想。"
+			}, {
+				id: 5,
+				content: "而这一段经历在之后也被她彻底埋藏…… "
+			}]
+		},
+		{
+			id: 6,
+			idView: 'img/character/06.png',
+			chrName: "香风幸（香風サチ）",
+			RMPN: "ka fu u sa chi",
+			chrCV: "？？？",
+			detail: [{
+				id: 1,
+				content: "心愿咖啡厅店长的爱女。"
+			}, {
+				id: 2,
+				content: "与麻美关系非常要好。"
+			}, {
+				id: 3,
+				content: "一直憧憬着想要成为麻美姐姐那样优秀的大人。"
+			}, ]
+		}
+	],
+	discribe_scr: 'img/discribe.png',
+	centerDialogVisible: false,
+	presentID: 0,
+	item: 0
+};
+*/
